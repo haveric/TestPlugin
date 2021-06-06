e6e7bf19a763fffea8815e8d6d7a1e2b585db8ef
Ryan Breuer <breuerryan@gmail.com>
2021-06-06 10:35:02 -0500

    Add spawner recipe

--- testplugin/src/main/java/haveric/testplugin/TestPlugin.java
+++ testplugin/src/main/java/haveric/testplugin/TestPlugin.java
@@ -4,16 +4,38 @@ package haveric.testplugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        ShapelessRecipe testRecipe = new ShapelessRecipe(new NamespacedKey(this, "test"), new ItemStack(Material.STONE));
+        ItemStack spawnerItemStack = new ItemStack(Material.SPAWNER);
+
+        BlockStateMeta blockStateMeta = (BlockStateMeta) spawnerItemStack.getItemMeta();
+        BlockState blockState = blockStateMeta.getBlockState();
+
+        CreatureSpawner spawner = (CreatureSpawner) blockState;
+        spawner.setSpawnedType(EntityType.CHICKEN);
+        spawner.setMinSpawnDelay(1);
+        spawner.setMaxSpawnDelay(3);
+        spawner.setMaxNearbyEntities(10);
+        spawner.setRequiredPlayerRange(5);
+        spawner.setSpawnRange(4);
+        spawner.setSpawnCount(3);
+
+        spawner.update();
+        blockStateMeta.setBlockState(spawner);
+        spawnerItemStack.setItemMeta(blockStateMeta);
+
+        ShapelessRecipe testRecipe = new ShapelessRecipe(new NamespacedKey(this, "test"), spawnerItemStack);
         testRecipe.addIngredient(Material.DIRT);
         testRecipe.addIngredient(Material.STONE);
 
