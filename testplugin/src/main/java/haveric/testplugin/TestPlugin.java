4b44edb3bd1b951b9a60ce3d32bba720e0b2b4d7
Ryan Breuer <breuerryan@gmail.com>
2021-06-13 22:24:11 -0500

    Test PlayerBucketEmptyEvent

--- testplugin/src/main/java/haveric/testplugin/TestPlugin.java
+++ testplugin/src/main/java/haveric/testplugin/TestPlugin.java
@@ -1,22 +1,22 @@
 package haveric.testplugin;
 
-
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        ShapelessRecipe testRecipe = new ShapelessRecipe(new NamespacedKey(this, "test"), new ItemStack(Material.STONE));
-        testRecipe.addIngredient(Material.DIRT);
-        testRecipe.addIngredient(Material.STONE);
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+    }
 
-        Bukkit.addRecipe(testRecipe);
+    @EventHandler
+    public void bucketEmpty(PlayerBucketEmptyEvent event) {
+        Bukkit.getLogger().info("Bucket: " + event.getBucket());
     }
 }
